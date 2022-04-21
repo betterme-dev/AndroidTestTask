0149f78a9cfa18b4dbe0eff402f1ddf662aa5abd
Konstantin Mikhailovskiy <konstieone@gmail.com>
2022-04-21 17:03:31 +0300

    Make movies remote store result in an error sometimes

--- data-movies/src/main/java/app/bettermetesttask/datamovies/repository/stores/MoviesRestStore.kt
+++ data-movies/src/main/java/app/bettermetesttask/datamovies/repository/stores/MoviesRestStore.kt
@@ -7,7 +7,13 @@ import kotlin.random.Random
 
 class MoviesRestStore @Inject constructor() {
 
+    private val statusCodes = listOf(200, 201, 202, 304, 400)
+
     suspend fun getMovies(): List<Movie> {
+        val statusCode = statusCodes.random()
+        if (statusCode >= 400) {
+            throw IllegalStateException("Did not manage to retrieve movies")
+        }
         delay(Random.nextLong(500, 3_000))
         return MoviesFactory.createMoviesList()
     }
