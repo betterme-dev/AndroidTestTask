2cf2448d8a06f3ba0a5de8acc0cb7da4bda37fa2
Serhii Hubar <serhii.hubar@betterme.world>
2025-02-27 17:47:08 +0200

    enh - add test examples

--- data-movies/src/test/java/app/bettermetesttask/datamovies/repository/MoviesRepositoryTest.kt
+++ data-movies/src/test/java/app/bettermetesttask/datamovies/repository/MoviesRepositoryTest.kt
@@ -6,4 +6,17 @@ import org.mockito.junit.jupiter.MockitoExtension
 @ExtendWith(MockitoExtension::class)
 internal class MoviesRepositoryTest {
 
+    // To mock dependencies - you can use the following syntax
+    //    private val someClass: String = mock()
+
+    // To test suspend function - you can use `runTest`
+    //    @Test
+    //    fun `test suspend function`() = runTest {
+    //       Verify something
+    //
+    //    }
+
+    // To verify a method is called - you can use the following syntax
+    //    verify(someObj).someMethod()
+
 }
--- domain-movies/src/test/java/app/bettermetesttask/domainmovies/interactors/GetMoviesUseCaseTest.kt
+++ domain-movies/src/test/java/app/bettermetesttask/domainmovies/interactors/GetMoviesUseCaseTest.kt
@@ -4,4 +4,18 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
 
 @ExtendWith(MockitoExtension::class)
-internal class GetMoviesUseCaseTest
+internal class GetMoviesUseCaseTest {
+
+    // To mock dependencies - you can use the following syntax
+    //    private val someClass: String = mock()
+
+    // To test suspend function - you can use `runTest`
+    //    @Test
+    //    fun `test suspend function`() = runTest {
+    //       Verify something
+    //
+    //    }
+
+    // To verify a method is called - you can use the following syntax
+    //    verify(someObj).someMethod()
+}
